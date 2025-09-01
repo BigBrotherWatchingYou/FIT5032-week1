897f0707eac620b06693adc4e387fdc43c43f922
BigBrotherWatchingYou <woshenyibande@gmail.com>
2025-09-01 21:15:46 +1000

    w4 efolio done

--- src/main.js
+++ src/main.js
@@ -6,4 +6,15 @@ import './style.css'
 // import bootstrap css
 import 'bootstrap/dist/css/bootstrap.min.css'
 
+
+import PrimeVue from 'primevue/config';
+import Aura from '@primeuix/themes/aura';
+
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura
+  }
+})
+
 createApp(App).mount('#app')
